32706e0573335a9bc5687fb2c385837f137b5179
Md Ahanaf Islam <ahanafislam@gmail.com>
2022-02-17 20:15:24 +0600

    Change Some Code

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 // Function For Get Input Value
 function getInputValue(id) {
     const inputTag = document.getElementById(id);
-    let inputValue = parseFloat(inputTag.value);
+    let inputValue = inputTag.value;
 
-    if(isNaN(inputValue)) {
+    if(inputValue === "") {
         inputValue = 0;
         inputTag.value = 0;
     }
 
-    return inputValue;
+    else if(isNaN(parseFloat(inputValue))) {
+        inputValue = 0;
+        document.getElementById('string-error').style.display = 'block';
+    }
+
+    return parseFloat(inputValue);
 }
 
 // Function For Show Error
@@ -29,10 +34,6 @@ function closeBtn(id) {
 function getBalance(expenses){
     const income = getInputValue('input-income');
     const balance = income - expenses;
-    // Error Message For Negative Income
-    if(income < 0){
-        errorMessage("Income "+ income +" is wrong input! Try with positive number");
-    }
 
     return balance;
 }
@@ -46,6 +47,7 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const clothesCost = getInputValue('input-clothes-cost');
     const resultUI = document.getElementById('result');
     resultUI.style.display = 'block';
+    document.getElementById('error-message').style.display = 'none';
 
     // Error Message For Negative Expense Amount
     if(foodCost < 0 || rentCost < 0 || clothesCost < 0){
@@ -62,9 +64,15 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const balanceValue = getBalance(totalExpensesValue);
     balanceUI.innerText = balanceValue;
 
-    // Error Message for over expense
-    if(income < totalExpensesValue) {
-        errorMessage("Your Expense Can't more then your income.");
+    // Error Message for over expense and Income
+    if(income < 0){
+        errorMessage("Income: "+ income +" is wrong input! Try with positive number");
+    }
+    else if(income == 0) {
+        errorMessage("Please enter your income as positive number for calculating.");
+    }
+    else if(income < totalExpensesValue) {
+        errorMessage("Your Expense Can't be more then your income.");
     }
 });
 
@@ -87,13 +95,21 @@ document.getElementById('save-btn').addEventListener('click', function() {
     // Error message for saving amount
     const balance = getBalance(totalExpense);
     const savingAmountResultUI = document.getElementById('saving-amount-result');
-    const savingErrorMessageUI = document.getElementById('saving-error-message')
+    const savingErrorMessageUI = document.getElementById('saving-error-message');
+
     if(balance < savingAmount) {
+        document.querySelector('#saving-error-message span').innerText = "You don't have enough money for saving";
         savingErrorMessageUI.style.display = 'block';
         savingAmountResultUI.style.display = 'none';
     }
     else {
         savingErrorMessageUI.style.display = 'none';
         savingAmountResultUI.style.display = 'block';
+    }
+
+    // Show error message if save input in negative number
+    if(save < 0) {
+        document.querySelector('#saving-error-message span').innerText = "Sorry! You Can't input any nagitive value";
+        savingErrorMessageUI.style.display = 'block';
     }
 });
