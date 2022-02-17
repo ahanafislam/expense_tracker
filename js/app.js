981ae4edecf1c73c13bce953392f25a76d8ea4a0
Md Ahanaf Islam <ahanafislam@gmail.com>
2022-02-17 15:20:17 +0600

    Give Some Error Msg for result

--- js/app.js
+++ js/app.js
@@ -7,13 +7,36 @@ function getInputValue(id) {
         inputValue = 0;
         inputTag.value = 0;
     }
+
     return inputValue;
+}
+
+// Function For Show Error
+function errorMessage(message) {
+    const errorMessageUI = document.getElementById('error-message');
+    const resultUI = document.getElementById('result');
+    document.querySelector('#error-message span').innerText = message;
+    errorMessageUI.style.display = 'block';
+    resultUI.style.display = 'none';
+}
+
+// Function For Close Btn
+function closeBtn(id) {
+    document.getElementById(id).style.display = 'none';
 }
 
 // Function For Calculate Balance
 function getBalance(expenses){
     const income = getInputValue('input-income');
     const balance = income - expenses;
+    // Error Message For Negative Income
+    if(income < 0){
+        errorMessage("Income "+ income +" is wrong input! Try with positive number");
+    }
+    // Error Message for over expense
+    else if(balance < 0) {
+        errorMessage("Your Expense Can't more then your income.");
+    }
 
     return balance;
 }
@@ -24,15 +47,23 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const foodCost = getInputValue('input-food-cost');
     const rentCost = getInputValue('input-rent-cost');
     const clothesCost = getInputValue('input-clothes-cost');
+    const resultUI = document.getElementById('result');
+
+    // Error Message For Negative Expense Value
+    if(foodCost < 0 || rentCost < 0 || clothesCost < 0){
+        errorMessage("Your Expenses Can't be any Nagative amount")
+    }
 
     // Calculate Total Expense
     const totalExpensesUI = document.getElementById('total-expenses');
     const totalExpensesValue = foodCost + rentCost + clothesCost;
     totalExpensesUI.innerText = totalExpensesValue;
+    resultUI.style.display = 'block';
 
     // Calculate Balance And Show In UI
     const balanceUI = document.getElementById('balance');
-    balanceUI.innerText = getBalance(totalExpensesValue);
+    const balanceValue = getBalance(totalExpensesValue);
+    balanceUI.innerText = balanceValue;
 });
 
 // Event Listener For Saving Amount
